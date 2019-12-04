d031c492937d0970f48859e7d4cb2348ec788e25
Michael Ilseman <milseman@apple.com>
2019-12-03 17:33:33 -0800

    WIP: messing around with tree

--- Sources/FileIO/Examples.swift
+++ Sources/FileIO/Examples.swift
@@ -7,6 +7,7 @@ public typealias Path = System.Path
 // For testing
 internal protocol SynchronousFileOperationExamples {
   // Returns number of bytes copied
+  @discardableResult
   static func copyFileContents(from: Path, into: (UInt8) throws -> ()) throws -> Int
 
   // Recommended default value of `true` for overwriteIfExists... Really want default protocol args.
@@ -18,6 +19,7 @@ internal protocol SynchronousFileOperationExamples {
   // ...
 }
 extension SynchronousFileOperationExamples {
+  @discardableResult
   static func copyFileContents(from: Path, into: (UInt8) throws -> ()) throws -> Int {
     fatalError()
   }
@@ -42,12 +44,33 @@ internal enum WithFoundation: SynchronousFileOperationExamples {
     if overwriteIfExits && fm.fileExists(atPath: destPathString) {
       try fm.removeItem(atPath: destPathString)
     }
-    try FileManager.default.copyItem(atPath: from.asString, toPath: destPathString)
+    try fm.copyItem(atPath: from.asString, toPath: destPathString)
+  }
+
+  static func copyFileContents(from: Path, into: (UInt8) throws -> ()) throws -> Int {
+    fatalError()
   }
 }
 
 internal enum WithSystem: SynchronousFileOperationExamples {
+  @discardableResult
+  static func copyFileContents(from: Path, into: (UInt8) throws -> ()) throws -> Int {
+    let bufferSize = 1024
+    let fd = try FileDescriptor.open(from, .readOnly)
+    var buffer = try fd.read(numBytes: bufferSize)
 
+    var len = buffer.count
+    var total = 0
+    repeat {
+      for byte in buffer {
+        try into(byte)
+      }
+      total += len
+      guard len == bufferSize else { return total }
+
+      len = try buffer.withUnsafeMutableBytes { try fd.read(into: $0) }
+    } while true
+  }
 }
 
 internal enum WithFileIO: SynchronousFileOperationExamples {
--- Sources/FileIO/main.swift
+++ Sources/FileIO/main.swift
@@ -1,7 +1,72 @@
 
 
 
-try WithFoundation.copyFile(from: Path("/tmp/a/bar.txt"), to: "/tmp/b/xbar.txt")
+//try WithFoundation.copyFile(from: Path("/tmp/a/bar.txt"), to: "/tmp/b/xbar.txt")
+
+
+func tryCopyFile() {
+  do {
+    try WithSystem.copyFileContents(from: Path("/tmp/a/bar.txt")) {
+      print(UnicodeScalar($0))
+    }
+  } catch {
+    dump(error)
+    fatalError()
+  }
+}
+tryCopyFile()
+
+// Do a `tree`-like printout
+
+let aPath = Path("/tmp/a")
+
+import Foundation
+
+enum EntryKind {
+  case directory
+  case file
+  case socket
+  case symlink
+
+  case unknown
+}
+extension EntryKind {
+  init(from: FileAttributeType) {
+    switch from {
+    case .typeDirectory: self = .directory
+    case .typeRegular: self = .file
+    case .typeSocket: self = .socket
+    case .typeSymbolicLink: self = .symlink
+
+    default: self = .unknown
+    }
+  }
+}
+
+guard let enumerator = FileManager.default.enumerator(atPath: aPath.asString) else {
+  fatalError("No FileManager enumerator provided for \(aPath.asString)")
+}
+print(enumerator.fileAttributes?[.type] ?? "none")
+
+print()
+for entry in enumerator {
+  guard let name = entry as? String,
+        let fileAttrType = enumerator.fileAttributes?[.type] as? FileAttributeType
+  else {
+    fatalError("wha?")
+  }
+
+  let kind = EntryKind(from: fileAttrType)
+  print("\(name), level \(enumerator.level), kind: \(kind)")
+
+  guard let posixPermissions = enumerator.fileAttributes?[.posixPermissions] as? NSNumber else {
+    fatalError("wha?")
+  }
+  let isExec = false
+  print(String(posixPermissions.int16Value, radix: 8))
+}
 
 print("Done")
 
+// tree:
+//   -F  Append  a `/' for directories, a `=' for socket files, a `*' for executable files, and a `|' for FIFO's
